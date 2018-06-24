59b27d7ff5c456dd745e0b44c8da2e98fb025117
mikalai <mikalai.churakou@gmail.com>
2018-06-24 13:23:56 +0300

    - docker and kubernetes

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting turvo-service "
+echo "********************************************************"
+java -jar /usr/local/turvo-service/@project.build.finalName@.jar
